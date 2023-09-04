b17ac3f34031c1701c870cb9f8d2e9fe8a814fb4
Edwinogechi <edwinogechi96@gmail.com>
2023-09-04 04:15:27 -0700

    Creating a file with content

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+#Creates a file with content in /tmp
+
+file { 'school':
+ensure  => 'present',
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet'
+}
