e0f24753e7188c94b1624effe6310cf669a5be52
Edwinogechi <edwinogechi96@gmail.com>
2023-11-03 13:10:42 +0300

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,5 +0,0 @@
-# code to install a package
-package{'flask':
-ensure   => '2.1.0',
-provider => 'pip3'
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# Kill process
-exec { 'pkill':
-  command  => 'pkill killmenow',
-  provider => shell,
-}
