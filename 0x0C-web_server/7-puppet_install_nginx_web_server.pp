1f60b7f39b8c2e41507153d770f9a619b4c8be3a
Edwinogechi <edwinogechi96@gmail.com>
2023-12-12 04:09:18 -0800

    configuring with nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# Installing nginx webserver (w/ Puppet)
+
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_name { 'install':
+  ensure => 'present',
+  path	 => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line	 => 'rewrite ^/redirect_me https://www.github.com/besthor permanent;',
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
